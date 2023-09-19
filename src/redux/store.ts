2c6e49c2e78bf5722c792c09b95df1d0f938b23f
Mamun172136 <ma1536204@gmail.com>
2023-09-19 19:47:54 +0600

    [done]  singlebook api slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import cartReducer from "./features/cart/cartSlice";
-import {api} from './api/apiSlice'
+import { api } from "./api/apiSlice";
 const store = configureStore({
   reducer: {
-  [api.reducerPath] = api.reducer
+    [api.reducerPath]: api.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
