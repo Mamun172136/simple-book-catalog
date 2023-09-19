b3f91ab101efaab624ec951c830a27e0c237fe53
Mamun172136 <ma1536204@gmail.com>
2023-09-20 03:53:23 +0600

    [done] get comment api slice

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,23 +3,33 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  tagTypes: ["comments"],
   endpoints: (builder) => ({
     getBooks: builder.query({
       query: () => "/books",
     }),
     singleBook: builder.query({
-      query: (id) => "/book/${id}",
+      query: (id) => `/book/${id}`,
     }),
 
     postComment: builder.mutation({
       query: ({ id, data }) => ({
-        url: "/comment/${id}",
+        url: `/comment/${id}`,
         method: "POST",
         body: data,
       }),
     }),
+
+    getComment: builder.query({
+      query: (id) => `/comment/${id}`, // Use backticks here
+      providesTags: ["comments"],
+    }),
   }),
 });
 
-export const { useGetBooksQuery, useSingleBookQuery, usePostCommentMutation } =
-  api;
+export const {
+  useGetBooksQuery,
+  useSingleBookQuery,
+  usePostCommentMutation,
+  useGetCommentQuery,
+} = api;
