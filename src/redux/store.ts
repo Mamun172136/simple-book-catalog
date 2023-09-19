eb4b9c05a9feed13b282d303b5fd601881e16b2c
Mamun172136 <ma1536204@gmail.com>
2023-09-19 19:04:29 +0600

    [done] add  getbooks api slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import cartReducer from "./features/cart/cartSlice";
-
+import {api} from './api/apiSlice'
 const store = configureStore({
-  reducer: {},
+  reducer: {
+  [api.reducerPath] = api.reducer
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
