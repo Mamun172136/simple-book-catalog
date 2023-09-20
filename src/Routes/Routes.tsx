beca0963b22a95b385bcda13c53fd5fa1bba83a3
Mamun172136 <ma1536204@gmail.com>
2023-09-21 01:25:32 +0600

    done add book route

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import AddBook from "../pages/AddBook/AddBook";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/addBook",
+        element: <AddBook></AddBook>,
       },
     ],
   },
