e4a186036cb3e40ffdbbbde0b832c88ea891a3b4
Mamun172136 <ma1536204@gmail.com>
2023-10-06 23:41:33 +0600

    add  bookReview page

--- src/pages/ProductReview/ProductReview.tsx
+++ src/pages/ProductReview/ProductReview.tsx
@@ -1,81 +1,81 @@
-// interface IProps {
-//   id: string;
-// }
+interface IProps {
+  id: string;
+}
 
-// import { ChangeEvent, FormEvent, useState } from "react";
-// import {
-//   useGetCommentQuery,
-//   usePostCommentMutation,
-// } from "../../redux/api/apiSlice";
+import { ChangeEvent, FormEvent, useState } from "react";
+import {
+  useGetCommentQuery,
+  usePostCommentMutation,
+} from "../../redux/api/apiSlice";
 
-// const ProductReview = ({ id }: IProps) => {
-//   const { data } = useGetCommentQuery(id, {
-//     refetchOnMountOrArgChange: true,
-//     pollingInterval: 30000,
-//   });
-//   const [postComment, { isLoading, isError, isSuccess }] =
-//     usePostCommentMutation();
+const ProductReview = ({ id }: IProps) => {
+  const { data } = useGetCommentQuery(id, {
+    refetchOnMountOrArgChange: true,
+    pollingInterval: 30000,
+  });
+  const [postComment, { isLoading, isError, isSuccess }] =
+    usePostCommentMutation();
 
-//   console.log(isLoading);
-//   console.log(isError);
-//   console.log(isSuccess);
+  console.log(isLoading);
+  console.log(isError);
+  console.log(isSuccess);
 
-//   const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
 
-//   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(inputValue);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(inputValue);
 
-//     const options = {
-//       id: id,
-//       data: { comment: inputValue },
-//     };
-//     console.log(options);
-//     postComment(options);
-//     setInputValue("");
-//   };
-//   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-//     setInputValue(event.target.value);
-//   };
+    const options = {
+      id: id,
+      data: { comment: inputValue },
+    };
+    console.log(options);
+    postComment(options);
+    setInputValue("");
+  };
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  };
 
-//   return (
-//     <div className="max-w-7xl mx-auto mt-5">
-//       <hr className="w-full border-t border-gray-300 my-2" />
-//       <form
-//         className="flex gap-5 items-center justify-center"
-//         onSubmit={handleSubmit}
-//       >
-//         {/* <Textarea
-//           className="min-h-[30px]"
-//           onChange={handleChange}
-//           value={inputValue}
-//         /> */}
+  return (
+    <div className="max-w-7xl mx-auto mt-5">
+      <hr className="w-full border-t border-gray-300 my-2" />
+      <form
+        className="flex gap-5 items-center justify-center"
+        onSubmit={handleSubmit}
+      >
+        {/* <Textarea
+          className="min-h-[30px]"
+          onChange={handleChange}
+          value={inputValue}
+        /> */}
 
-//         <textarea
-//           onChange={handleChange}
-//           value={inputValue}
-//           placeholder="write your review"
-//           className="  textarea textarea-bordered textarea-xs w-full max-w-xs"
-//         ></textarea>
-//         <button className="btn btn-neutral" type="submit">
-//           {" "}
-//           submit{" "}
-//         </button>
-//       </form>
+        <textarea
+          onChange={handleChange}
+          value={inputValue}
+          placeholder="write your review"
+          className="  textarea textarea-bordered textarea-xs w-full max-w-xs"
+        ></textarea>
+        <button className="btn btn-neutral" type="submit">
+          {" "}
+          submit{" "}
+        </button>
+      </form>
 
-//       <div className="mt-10">
-//         {data?.comments?.map((comment: string, index: number) => (
-//           <div key={index} className="flex gap-3 items-center mb-5">
-//             {/* <Avatar>
-//               <AvatarImage src="https://github.com/shadcn.png" />
-//               <AvatarFallback>CN</AvatarFallback>
-//             </Avatar> */}
-//             <li> user review: {comment}</li>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="mt-10">
+        {data?.comments?.map((comment: string, index: number) => (
+          <div key={index} className="flex gap-3 items-center mb-5">
+            {/* <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar> */}
+            <li> user review: {comment}</li>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default ProductReview;
+export default ProductReview;
